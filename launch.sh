fdfb47d3acdfe6ff8c7d8042b3c3f0285d932a3d
ravikishan.69 <ravikishan63392@gmail.com>
2024-11-02 00:38:07 +0530

    Update Launch the database locations

--- launch.sh
+++ launch.sh
@@ -10,16 +10,16 @@ sleep 0.1
 
 go install -v
 
-distributedKV -db-location=moscow.db -http-addr=127.0.0.2:8080 -config-file=sharding.toml -shard=Moscow &
-distributedKV -db-location=moscow-r.db -http-addr=127.0.0.22:8080 -config-file=sharding.toml -shard=Moscow -replica &
+distributedKV -db-location=Mumbai.db -http-addr=127.0.0.2:8080 -config-file=sharding.toml -shard=Mumbai &
+distributedKV -db-location=Mumbai-r.db -http-addr=127.0.0.22:8080 -config-file=sharding.toml -shard=Mumbai -replica &
 
-distributedKV -db-location=minsk.db -http-addr=127.0.0.3:8080 -config-file=sharding.toml -shard=Minsk &
-distributedKV -db-location=minsk-r.db -http-addr=127.0.0.33:8080 -config-file=sharding.toml -shard=Minsk -replica &
+distributedKV -db-location=Delhi.db -http-addr=127.0.0.3:8080 -config-file=sharding.toml -shard=Delhi &
+distributedKV -db-location=Delhi-r.db -http-addr=127.0.0.33:8080 -config-file=sharding.toml -shard=Delhi -replica &
 
-distributedKV -db-location=kiev.db -http-addr=127.0.0.4:8080 -config-file=sharding.toml -shard=Kiev &
-distributedKV -db-location=kiev-r.db -http-addr=127.0.0.44:8080 -config-file=sharding.toml -shard=Kiev -replica &
+distributedKV -db-location=Chennai.db -http-addr=127.0.0.4:8080 -config-file=sharding.toml -shard=Chennai &
+distributedKV -db-location=Chennai-r.db -http-addr=127.0.0.44:8080 -config-file=sharding.toml -shard=Chennai -replica &
 
-distributedKV -db-location=tashkent.db -http-addr=127.0.0.5:8080 -config-file=sharding.toml -shard=Tashkent &
-distributedKV -db-location=tashkent-r.db -http-addr=127.0.0.55:8080 -config-file=sharding.toml -shard=Tashkent -replica &
+distributedKV -db-location=Bangalore.db -http-addr=127.0.0.5:8080 -config-file=sharding.toml -shard=Bangalore &
+distributedKV -db-location=Bangalore-r.db -http-addr=127.0.0.55:8080 -config-file=sharding.toml -shard=Bangalore -replica &
 
 wait
